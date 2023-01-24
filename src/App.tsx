c3ac7c001388e39445eeec737588d60b70c8cc0d
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-01-24 16:31:21 -0300

    :sparkles: feat: routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-    </div>
+    <BrowserRouter>
+      <Router/>
+    </BrowserRouter>
   );
 }
 
