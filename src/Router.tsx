c7aa03dc9b1baf2bab272268eee79f46c4e4da0e
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-01-26 18:10:11 -0300

    :sparkles: feat: sign-up route.

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 
 import { HomePage } from "./pages/HomePage/HomePage"
 import { SignInPage } from "./pages/SignInPage/SignInPage"
+import { SignUpPage } from "./pages/SignUpPage/SignUpPage"
 
 export function Router() {
   return(
 		<Routes>
 			<Route path="/" element={ <HomePage /> }/>
 			<Route path="/sign-in" element={<SignInPage />}/>
+			<Route path="/sign-up" element={<SignUpPage />}/>
 		</Routes>
 	)
 }
