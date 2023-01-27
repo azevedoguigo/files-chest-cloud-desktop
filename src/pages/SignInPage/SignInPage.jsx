9c7783f30e7ee4e46d6fb2c5ccedd601e113b328
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-01-27 18:24:02 -0300

    :sparkles: feat: sign in form validation.

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
 import { api } from "../../services/api"
 
 import "./SignInPage.css"
 
 export function SignInPage() {
+  const { 
+    register, 
+    handleSubmit,  
+    formState: { errors }
+  } = useForm()
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
-  async function handleSubmit(event) {
+  async function onSubmit(event) {
     event.preventDefault()
 
     const respose = await api.post('/users/signin', {
@@ -27,20 +33,26 @@ export function SignInPage() {
 
   return(
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={ handleSubmit(onSubmit(event)) }>
         <h3>SignIn</h3>
+
         <input 
           type="email" 
           name="email" 
           placeholder="Your Email" 
+          {...register("email", { required: true })}
           onChange={event => setEmail(event.target.value)}
         />
+        {errors.email && <p>Email is required!</p>}
+
         <input 
           type="password" 
           name="password" 
           placeholder="Your Password" 
+          {...register("password", { required: true })}
           onChange={event => setPassword(event.target.value)}
         />
+        {errors.password && <p>Password is required!</p>}
 
         <span>Don't have an account yet? <Link to={"/sign-up"}>SignUp</Link></span>
 
